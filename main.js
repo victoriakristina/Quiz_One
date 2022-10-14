3c6b6179300bb88df3a70a7921ca9b5f7cfdc79f
victoriakristina <victoriaercin@gmail.com>
2022-10-14 22:36:34 +0200

    la till variabler i js

---
+++ main.js
@@ -0,0 +1,16 @@
+function check(){
+
+let question1 = document.quiz.question1.value;
+let question1 = document.quiz.question2.value;
+let question1 = document.quiz.question3.value;
+let question1 = document.quiz.question4.value;
+let question1 = document.quiz.question5.value;
+let question1 = document.quiz.question6.value;
+let question1 = document.quiz.question7.value;
+let question1 = document.quiz.question8.value;
+let question1 = document.quiz.question9.value;
+let question1 = document.quiz.question10.value;
+   
+}
+
+document.getElementById("after_submit").style.visibility = "visible";
